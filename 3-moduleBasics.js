2d0e0aea690831e0be14cc1014818fd5607fabae
Himani Jangid <himanijangid07@gmail.com>
2025-06-03 23:47:34 +0530

    os module

--- 3-moduleBasics.js
+++ 3-moduleBasics.js
@@ -3,10 +3,14 @@
 
 const names = require('./4-names')
 const sayHi = require('./5-utils')
+const data = require('./6-alternativeWay')
+require('./7-mindGrenade')
+
+// console.log(data)
 
 // console.log(names)
 
-sayHi('sumit')
-sayHi(names.himani)
-sayHi(names.sweetu)
+// sayHi('sumit')
+// sayHi(names.himani)
+// sayHi(names.sweetu)
 // sayHi(sayHi("vishal")) // first time printed vishal, and then second time undefined
