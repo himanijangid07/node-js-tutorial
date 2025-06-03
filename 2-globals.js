58b43d27f5594cc57195af78c4c1361e24a23727
Himani Jangid <himanijangid07@gmail.com>
2025-06-03 13:19:14 +0530

    globals in node js

---
+++ 2-globals.js
@@ -0,0 +1,13 @@
+// GLOBAL - NO WINDOW!!!
+
+// __dirname = provides the name of the current directory
+// __filename = provides the name of the current file
+// require = function to use modules (CommonJS)
+// module = info about current module (file)
+// process = info about the env where the program is being executed
+
+console.log(__dirname)
+
+setInterval(() => {
+    console.log("hello world")
+}, 1000)
